15ed0f7fd63fdb448de8e2225f2ea13c44698feb
YuriiPtyts <yuraptyts@gmail.com>
2026-03-01 01:51:31 +0100

    Added localStorage usage for JSON

--- app.js
+++ app.js
@@ -1,12 +1,25 @@
+// localStorage.clear();
+
+
 const searchBarInput = document.querySelector(".search-bar");
 const addButton = document.querySelector(".add-button");
 const tasksContainer = document.querySelector(".tasks-container");
 const tasksRemaining = document.querySelector(".tasks-remaining");
 const tasksAmount = document.querySelector(".tasks-amount");
-let tasksArr = [];
+
+let tasksArr = JSON.parse(localStorage.getItem("tasksList")) || [];
 
 let tasksCounter = 0;
 let taskId = 0;
+
+if(tasksArr.length > 0) {
+    tasksArr.forEach(object => {
+        toDo(object);
+    });
+    changeRemaining();
+    taskId = tasksArr[tasksArr.length-1].id;
+    console.log(taskId);
+}
 
 // const task = searchBarInput.value;
 
@@ -46,6 +59,16 @@ addButton.addEventListener("click", () =>{
 });
 
 function toDo(task) {
+    console.log(typeof task);
+    
+    let taskObjectId,taskObjectTask;
+
+    if (typeof task === "object" && task !== null) {
+        taskObjectId = task.id;
+        taskObjectTask = task.taskName;
+        console.log(taskObjectId, taskObjectTask);
+    }
+
     tasksCounter++;
 
     let noTasks = document.querySelector(".no-tasks");
@@ -54,41 +77,50 @@ function toDo(task) {
     }
     let newDiv = document.createElement("div");
     newDiv.className = "task";
-    newDiv.id = `task${++taskId}`
+    newDiv.id = `task${taskObjectId || ++taskId}`
     tasksContainer.appendChild(newDiv);
 
     let newCheckbox = document.createElement("input");
     newCheckbox.type = "checkbox";
     newCheckbox.className = "checkbox";
-    newCheckbox.id = `checkbox${taskId}`;
+    newCheckbox.id = `checkbox${taskObjectId || taskId}`;
+    if (task.completed) {
+        newCheckbox.checked = true;
+    }
     newDiv.appendChild(newCheckbox);
 
-    const newTask = {
-        id: taskId,
-        task: task
-    };
-
-    tasksArr.push(newTask);
-    console.log(tasksArr);
+    if(!(tasksArr.some(task => task.id == taskObjectId))){
+        const newTask = {
+            id: taskId,
+            taskName: taskObjectTask || task,
+            completed: false
+        };
+        tasksArr.push(newTask);
+        console.log(tasksArr);
+    }
 
     let newLabel = document.createElement("label");
-    newLabel.id = `label${taskId}`;
-    newLabel.htmlFor = `checkbox${taskId}`;
-    newLabel.appendChild(document.createTextNode(task));
+    newLabel.id = `label${taskObjectId || taskId}`;
+    newLabel.htmlFor = `checkbox${taskObjectId || taskId}`;
+    newLabel.appendChild(document.createTextNode(taskObjectTask || task));
+    if (task.completed) {
+        newLabel.style.textDecoration = "line-through";
+    }
     newDiv.appendChild(newLabel);
 
     let newDeleteButton = document.createElement("button");
     newDeleteButton.className = "delete-button";
-    newDeleteButton.id =`deleteButton${taskId}`;
+    newDeleteButton.id =`deleteButton${taskObjectId || taskId}`;
     newDeleteButton.innerText = "X";
     newDiv.appendChild(newDeleteButton);
 
-    let deleteButton = document.querySelector(`#deleteButton${taskId}`);
+    let deleteButton = document.querySelector(`#deleteButton${taskObjectId || taskId}`);
     deleteButton.addEventListener("click", ()=> {
         newDiv.remove();
 
         const deleteButtonId =getId(deleteButton);
         taskObjectDelete(deleteButtonId);
+        saveToLocalStorage();
 
         tasksCounter--;
         changeRemaining();
@@ -102,15 +134,25 @@ function toDo(task) {
         }
     });
 
-    let checkbox = document.querySelector(`#checkbox${taskId}`);
-    let label = document.querySelector(`#label${taskId}`);
+    let checkbox = document.querySelector(`#checkbox${taskObjectId || taskId}`);
+    let label = document.querySelector(`#label${taskObjectId || taskId}`);
     checkbox.addEventListener('change', (event) => {
         if (event.target.checked) {
             label.style.textDecoration = "line-through";
         } else {
             label.style.textDecoration = "none";
         }
+
+        const currentId = taskObjectId || taskId;
+        const taskIndex = tasksArr.findIndex(task => task.id === currentId);
+        if (taskIndex !== -1) {
+            tasksArr[taskIndex].completed = event.target.checked;
+            saveToLocalStorage(); 
+        }
     });
+
+    saveToLocalStorage();
+
 }
 
 function changeRemaining() {
@@ -126,5 +168,9 @@ function getId(object) {
     const idNumberAsObject = fullId.match(/\d+\.?\d*/g);
     const idNumber = idNumberAsObject.toString();
     return Number(idNumber);
+}
+
+function saveToLocalStorage() {
+    localStorage.setItem("tasksList", JSON.stringify(tasksArr));
 }
 
